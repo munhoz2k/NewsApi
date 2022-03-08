de072c88e4d6670007d49e69846b06bee87d9ed6
munhoz2k <lucasmunhozarruda@gmail.com>
2022-03-08 01:04:00 -0300

    WebScrapper

--- src/providers/InvestingPuppeteer.ts
+++ src/providers/InvestingPuppeteer.ts
@@ -3,17 +3,26 @@ import { PageContent } from '../interfaces/PageContent'
 
 export class InvestingPuppeteer {
     
-    async scrapper(): Promise<PageContent> {
+    async scrapper(): Promise<PageContent | null> {
+        const originalArticle = 'js-article-item articleItem   '
+
         //Inicia o navegador do Puppeteer na página de notícias
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
         await page.goto('https://br.investing.com/news/stock-market-news')
         
-        // Acessa o link da última notícia
-        const link = await page.evaluate(() => {
+        // Acessa o link da última notícia        
+        const { link, articleType } = await page.evaluate(() => {
+            const articleType = document.querySelector('div.largeTitle article').className
             const link = document.querySelector('div.largeTitle div.textDiv a').getAttribute('href')
-            return link
+            return { link, articleType }
         })
+
+        // Filtra por artigos de notícias originais do site: br.investing.com
+        if (articleType !== originalArticle) {
+            return null
+        }
+
         const fullLink = `https://br.investing.com${link}`
         await page.goto(`${fullLink}`, { timeout: 0 })
         
